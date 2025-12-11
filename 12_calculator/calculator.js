5a1a74b3d58f30e7ad027463996a2a55c2fd2158
AliceFantasia <51605721+AliceFantasia@users.noreply.github.com>
2025-12-11 16:11:48 +0700

    Update calculator.js
    Added code to the functions

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, index) => sum + index, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a,b) {
+	return a * b;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let factNum = 1;
+	for(let i = 1; i < factNum ; i++){
+		factNum *= i;
+	}
+	return factNum; 
 };
 
 // Do not edit below this line
